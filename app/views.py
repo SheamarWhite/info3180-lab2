64950c8a95eb877c43eb2ca3c0b1d8ae14bdf789
SheamarWhite <sheam2002@LAPTOP-52FS6K8S>
2024-02-21 22:45:51 -0500

    Finished Webpage

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
@@ -17,6 +17,9 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html', now=now)
 
 ###
 # The functions below should be applicable to all Flask apps.
@@ -45,3 +48,9 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+def format_date_joined(date):
+    return date.strftime('%B, %Y')
+
+now = format_date_joined(datetime.datetime.now())
+print("This is date: ", now)
